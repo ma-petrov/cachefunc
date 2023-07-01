042db0993630f0875ca5227d36796cfeddd45b90
Petrov Mikhail <petrov.ma@icloud.com>
2023-07-01 23:01:23 +0300

    refacotring, add redis cache

--- cachefunc/main.py
+++ cachefunc/main.py
@@ -1,89 +1,36 @@
-from base64 import b64encode
-from collections import namedtuple
 from contextlib import suppress
-from datetime import datetime, timedelta
+from datetime import timedelta
 from functools import wraps
-from logging import getLogger
-from typing import Any, Callable, Coroutine, Hashable, Union
+from typing import Any, Callable, Coroutine
 
-
-logger = getLogger(__name__)
-
-
-DEFAULT_TIMEOUT = timedelta(hours=1)
-
-
-class BaseCache:
-    class NotCached(Exception): ...
-    def __init__(self): ...
-    def get(self, key: Hashable) -> Union[Any, None]: ...
-    def set(self, key: Hashable, data: Any, timeout: timedelta) -> None: ...
-
-
-class DictCache(BaseCache):
-    Row = namedtuple('Row', 'data expiretime')
-
-    def __init__(self):
-        self.cache = dict()
-
-    def get(self, key: Hashable) -> Union[Any, None]:
-        with suppress(KeyError):
-            return self.cache[key].data
-        raise BaseCache.NotCached()
-    
-    def set(self, key: Hashable, data: Any, timeout: timedelta) -> None:
-        self._clear_expired()
-        self.cache[key] = self.Row(data, datetime.now() + timeout)
-
-    def clear(self) -> None:
-        self.cache.clear()
-
-    def _clear_expired(self) -> None:
-        now = datetime.now()
-        for key, row in self.cache.items():
-            if row.expiretime < now:
-                del self.cache[key]
+from cachefunc.cache.base import BaseCache, NotCached
+from cachefunc.cache.dict_cache import DictCache
 
 
 default_cache = DictCache()
 
 
-def _get_key(func: Union[Callable, Coroutine]) -> str:
-    return func.__qualname__
-
-
-def cachefunc(
-    cache: BaseCache = default_cache,
-    timeout: timedelta = DEFAULT_TIMEOUT,
-) -> Callable:
+def cachefunc(cache: BaseCache = default_cache, **cache_kwargs,) -> Callable:
     def decorator(func: Callable) -> Callable:
-        key = _get_key(func)
-        print(key)
         @wraps(func)
         def wrapper(*args, **kwargs) -> Any:
-            with suppress(BaseCache.NotCached):
-                return cache.get(key)
+            with suppress(NotCached):
+                return cache.get(func, args, kwargs)
             result = func(*args, **kwargs)
-            cache.set(key, result, timeout)
+            cache.set(func, args, kwargs, result, **cache_kwargs)
             return result
         return wrapper
     return decorator
 
 
-def cachecoro(
-    cache: BaseCache = default_cache,
-    timeout: timedelta = DEFAULT_TIMEOUT,
-) -> Callable:
+def cachecoro(cache: BaseCache = default_cache, **cache_kwargs,) -> Callable:
     def decorator(func: Coroutine) -> Coroutine:
-        key = _get_key(func)
         @wraps(func)
         async def async_wrapper(*args, **kwargs) -> Any:
-            with suppress(BaseCache.NotCached):
-                return cache.get(key)
+            with suppress(NotCached):
+                return cache.get(func, args, kwargs)
             result = await func(*args, **kwargs)
-            cache.set(key, result, timeout)
+            cache.set(func, args, kwargs, result, **cache_kwargs)
             return result
         return async_wrapper
     return decorator
-
-
--- tests/test_main.py
+++ tests/test_main.py
@@ -18,6 +18,12 @@ def other_target_func(testcall: MagicMock) -> str:
     return OTHER_RESULT
 
 
+@cachefunc()
+def target_func_with_args(testcall: MagicMock, arg: int) -> str:
+    testcall(f'called with arg: {arg}')
+    return arg
+
+
 class A:
     @cachefunc()
     def target_func(self, testcall: MagicMock) -> str:
@@ -29,52 +35,50 @@ class B:
     def target_func(self, testcall: MagicMock) -> str:
         testcall('method of B was called')
         return OTHER_RESULT
-    
-
-def test_cachefunc__no_data_in_cache():
-    default_cache.clear()
-    testcall = MagicMock()
-
-    result = target_func(testcall)
-    testcall.assert_called_once_with('target func was called')
-    assert result == RESULT
 
 
 def test_cachefunc__fetch_data_from_cache():
     default_cache.clear()
     testcall = MagicMock()
 
-    result = target_func(testcall)
-    result = target_func(testcall)
+    assert target_func(testcall) == RESULT
+    assert target_func(testcall) == RESULT
     testcall.assert_called_once_with('target func was called')
-    assert result == RESULT
     
 
 def test_cachefunc__unique_key():
     default_cache.clear()
     testcall = MagicMock()
 
-    result = target_func(testcall)
-    other_result = other_target_func(testcall)
+    assert target_func(testcall) == RESULT
+    assert other_target_func(testcall) == OTHER_RESULT
     testcall.assert_has_calls([
         call('target func was called'),
         call('other target func was called'),
     ])
-    assert result == RESULT
-    assert other_result == OTHER_RESULT
     
 
 def test_cachefunc__unique_key_class_method():
     default_cache.clear()
     testcall = MagicMock()
 
-    result = A().target_func(testcall)
-    other_result = B().target_func(testcall)
+    assert A().target_func(testcall) == RESULT
+    assert B().target_func(testcall) == OTHER_RESULT
     testcall.assert_has_calls([
         call('method of A was called'),
         call('method of B was called'),
     ])
-    assert result == RESULT
-    assert other_result == OTHER_RESULT
+    
 
+def test_cachefunc__distinct_arg_cache():
+    default_cache.clear()
+    testcall = MagicMock()
 
+    assert target_func_with_args(testcall, 1) == 1
+    assert target_func_with_args(testcall, 1) == 1
+    assert target_func_with_args(testcall, 2) == 2
+    assert target_func_with_args(testcall, 2) == 2
+    testcall.assert_has_calls([
+        call('called with arg: 1'),
+        call('called with arg: 2'),
+    ])
