6726162c94e4c4108c70ef0edf04019903b42f6f
Petrov Mikhail <petrov.ma@icloud.com>
2023-07-04 18:55:03 +0300

    fix log module

--- cachefunc/log.py
+++ cachefunc/log.py
@@ -1,33 +1,9 @@
-from logging import Logger, getLogger
-from logging.config import dictConfig
+from logging import Logger
 
-
-LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': { 
-        'default': { 
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': { 
-        'default': { 
-            'level': 'INFO',
-            'formatter': 'default',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'root': {
-            'handlers': ['default'],
-            'level': 'WARN',
-        }
-    }
-}
-
-
-dictConfig(LOGGING_CONFIG)
-
+try:
+    from structlog import getLogger
+except ModuleNotFoundError:
+    from logging import getLogger
 
 def get_logger(__name__: str) -> Logger:
     return getLogger(__name__)
